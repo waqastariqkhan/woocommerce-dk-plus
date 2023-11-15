fd97f983a30fae427108e1379a02ca2b9f5120aa
Waqas Tariq <waqastariq0987@gmail.com>
2023-11-16 00:14:48 +0500

    basic layout and install wordpress coding standards

--- wc-dk-plus.php
+++ wc-dk-plus.php
@@ -2,13 +2,12 @@
 /**
  * The plugin bootstrap file
  *
- *
  * @link              https://github.com/waqastariqkhan
  * @since             1.0.0
  * @package           Vogue_Erp
  *
  * @wordpress-plugin
- * Plugin Name:       Woocommerce DK Plus - Invoice Generator   
+ * Plugin Name:       Woocommerce DK Plus - Invoice Generator
  * Plugin URI:        https://github.com/waqastariqkhan/woocommerce-dk-plus
  * Description:       Generate an invoice in the DK plus management system when an order is placed in the Woocommerce
  * Version:           1.0.0
@@ -19,5 +18,13 @@
  * Text Domain:       wc-dk-plus
  * Domain Path:       /languages
  */
- 
- 
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+class WC_DK_PLUS{
+    private $api_endpoint;
+    private $api_key;
+}
