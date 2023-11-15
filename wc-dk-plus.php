41364988c574917b8214a02817e2ca99f0952ec7
Waqas Tariq <waqastariq0987@gmail.com>
2023-11-15 22:28:28 +0500

    update plugin information

--- wc-dk-plus.php
+++ wc-dk-plus.php
@@ -1,1 +1,23 @@
 <?php
+/**
+ * The plugin bootstrap file
+ *
+ *
+ * @link              https://github.com/waqastariqkhan
+ * @since             1.0.0
+ * @package           Vogue_Erp
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Woocommerce DK Plus - Invoice Generator   
+ * Plugin URI:        https://github.com/waqastariqkhan/woocommerce-dk-plus
+ * Description:       Generate an invoice in the DK plus management system when an order is placed in the Woocommerce
+ * Version:           1.0.0
+ * Author:            Waqas Tariq
+ * Author URI:        https://github.com/waqastariqkhan
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wc-dk-plus
+ * Domain Path:       /languages
+ */
+ 
+ 
